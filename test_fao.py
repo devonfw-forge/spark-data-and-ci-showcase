ddd7892f73f8fcc5345695655759b9e2975a3424
SergioTJ11 <52447686+SergioTJ11@users.noreply.github.com>
2019-07-09 19:07:00 +0200

    Update test_fao.py

--- test_fao.py
+++ test_fao.py
@@ -1,21 +1,22 @@
 from unittest import TestCase
 from FAO import Fao
 
+
 class TestFao(TestCase):
-    def setUP(self,data):
+    def setUp(self):
         self.data = Fao()
 
     def test_countries(self):
         self.assertEqual(self.data.countries()[0], "Afghanistan")
 
     def test_products(self):
-        self.asserEqual(self.data.products("Afghanistan")[0], "Wheat and products")
+        self.assertEqual(self.data.products("Afghanistan")[0], "Wheat and products")
 
     def test_min(self):
-        self.assertEqual(self.data.min(["Afghanistan"], ['Y1961', 'Y2013']), )
+        self.assertEqual(self.data.min(["Afghanistan"], [2010, 2013]), "a completer")
 
     def test_max(self):
-        self.assertEqual(self.data.max(["Afghanistan"], ['Y1961', 'Y2013']), ["Wheat and products", "Y2013"])
+        self.assertEqual(self.data.max(["Afghanistan"], [2010, 2013]), {'Afghanistan': [['Cereals - Excluding Beer', 'Y2013', 5495], []]})
 
     def test_av(self):
-        self.assertEqual(self.data.av(["Afghanistan"], ['Y1961', 'Y1965'], "Wheat and products"), 1889.8)
+        self.assertEqual(self.data.av(["Afghanistan"], [1961, 1965], "Wheat and products"), 1889.8)
