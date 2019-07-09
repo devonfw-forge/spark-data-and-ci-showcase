c84c13b87c5dce209d42294c115f252b2d2f1eb4
PierreCapgemini <52694142+PierreCapgemini@users.noreply.github.com>
2019-07-09 13:19:17 +0200

    Update FAO.py
    Adding of the products function

--- FAO.py
+++ FAO.py
@@ -13,6 +13,17 @@ class Fao:
             if element["Area"] not in country_list:
                 country_list.append(element["Area"])
         return country_list
+    
+    def products(self, nameCountry): #function to have a list a product given the name country
+        productList = []
+        for elt in self.dataBase:
+            if elt["Area"] == nameCountry:
+                if elt["Item"] not in productList:
+                    productList.append(elt["Item"])
+        print(productList)
 
+if __name__ == '__main__':
 
+    FAO1 = FAO()
+    FAO1.products("Zimbabwe")
 
