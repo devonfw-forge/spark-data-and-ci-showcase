420dba7d1ca404e3dae117719b776627cbfd6ae8
SergioTJ11 <52447686+SergioTJ11@users.noreply.github.com>
2019-07-10 11:59:34 +0200

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -13,16 +13,16 @@ class TestApi(TestCase):
         self.assertNotEqual(self.data.countries()[1], "Andorra")
 
     def test_population(self):
-        self.assertEqual(self.data.population("Aruba")[0], "54211")
+        self.assertEqual(self.data.population('Aruba',['1960','1962'])[0], "54211")
 
     def test_population2(self):
-        self.assertNotEqual(self.data.population("Aruba")[2], "59063")
+        self.assertNotEqual(self.data.population('Aruba',['1960','1962'])[2], "59063")
 
     def test_countries_pop(self):
-        self.assertEqual(self.data.countries_pop(["Aruba"])[0][1][0], "54211")
+        self.assertEqual(self.data.countries_pop(['Aruba','Andorra'],['1960','1962'])[0][1][0], "54211")
 
     def test_countries_pop2(self):
-        self.assertNotEqual(self.data.countries_pop(["Aruba"])[0][1][1], "57032")
+        self.assertNotEqual(self.data.countries_pop(['Aruba','Andorra'],['1960','1962'])[0][1][1], "57032")
    
     def test_growth(self):
         self.assertEqual(self.data.growth(["Aruba","Afghanistan"], ["1960", "1965"]), ["Aruba:3149", "Afghanistan:959347"])
