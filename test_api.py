165b4a58ef7a7f4ae255a93604af33b7f38dcc98
SergioTJ11 <52447686+SergioTJ11@users.noreply.github.com>
2019-07-10 11:21:28 +0200

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,8 +1,29 @@
 from unittest import TestCase
+from API import Api
 
 
 class TestApi(TestCase):
+    def setUp(self):
+        self.data = Api()
 
+    def test_countries(self):
+        self.assertEqual(self.data.countries()[0], "Aruba")
+
+    def test_countries2(self):
+        self.assertNotEqual(self.data.countries()[1], "Andorra")
+
+    def test_population(self):
+        self.assertEqual(self.data.population("Aruba")[0], "54211")
+
+    def test_population2(self):
+        self.assertNotEqual(self.data.population("Aruba")[2], "59063")
+
+    def test_countries_pop(self):
+        self.assertEqual(self.data.countries_pop(["Aruba"])[0][1][0], "54211")
+
+    def test_countries_pop2(self):
+        self.assertNotEqual(self.data.countries_pop(["Aruba"])[0][1][1], "57032")
+   
    def test_growth(self):
         self.assertEqual(self.data.growth(["Aruba"], ["1960", "1961"]), 1227)
 
