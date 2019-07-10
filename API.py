7d6509ee3fbd5778dfed23fc57e107ade50be05f
SergioTJ11 <52447686+SergioTJ11@users.noreply.github.com>
2019-07-10 10:36:41 +0200

    Update API.py

--- API.py
+++ API.py
@@ -5,9 +5,16 @@ class Api:
 
     def __init__(self):
         self.dataBase = []
+        self.dataHead = []
         with open("API_SP.POP.TOTL_DS2_en_csv_v2_162.csv") as csv_file:
-            for column in csv.reader(csv_file, delimiter=','):
-                self.dataBase.append(column)
-        self.dataBase = self.dataBase[4:-1]
+            for row in csv.reader(csv_file, delimiter=','):
+                self.dataBase.append(row)
+        self.dataHead = self.dataBase[4]
+        self.dataBase = self.dataBase[5:-1]
 
+    def countries(self):
+        country_list = []
+        for country in self.dataBase:
+            country_list.append(country[0])
+        return country_list
 
