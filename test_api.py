9d83e23ea01024b075c21f8ab8cf449e17b8f779
PierreCapgemini <52694142+PierreCapgemini@users.noreply.github.com>
2019-07-11 10:52:54 +0200

    Adding final tests to Testing Class --FINAL VERSION--

--- test_api.py
+++ test_api.py
@@ -29,3 +29,15 @@ class TestApi(TestCase):
 
     def test_growth2(self):
         self.assertNotEqual(self.data.growth(["Aruba"], ["1960", "1961"]),["Aruba:3149"])
+        
+    def test_minpoblacion(self):
+        self.assertEqual(self.data.minPoblacion(["Armenia", "Comoros", "Bahrain"],['1960', '1970']), [1874121, 191121, 162427])
+
+    def test_minpoblacion2(self):
+        self.assertNotEqual(self.data.minPoblacion(["Brazil", "Barbados", "Gabon"],['1960', '1980']), [1874121, 191121, 162427])
+
+    def test_maxpoblacion(self):
+        self.assertEqual(self.data.maxPoblacion(["Armenia", "Comoros", "Zimbabwe"], ['1970', '1980']),[3049109, 297447, 7160023])
+
+    def test_maxpoblacion2(self):
+        self.assertNotEqual(self.data.maxPoblacion(["Brazil", "Austria", "Gabon"], ['1960', '1980']),[4587, 191121, 162427])
