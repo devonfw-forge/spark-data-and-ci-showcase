beabd499097c3f34be96892509fa5ba3e6f04f58
SergioTJ11 <52447686+SergioTJ11@users.noreply.github.com>
2019-07-11 10:52:15 +0200

    Add files via upload

---
+++ SQL.py
@@ -0,0 +1,25 @@
+from sqlalchemy import *
+from sqlite3 import*
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+data_tables = create_engine('sqlite:///world-gdp.db')
+
+Session = sessionmaker(bind=data_tables)
+session = Session()
+
+Base = declarative_base()
+
+
+class Gdp(Base):
+   __tablename__ = 'gdp'
+
+   id = Column(Integer, primary_key=True)
+   CountryCode = Column(String)
+   Year = Column(Integer)
+   gdp = Column(Integer)
+   growth = Column(Integer)
+
+query = session.query(Gdp)
+
+print(query.filter_by(CountryCode="ABW"))
