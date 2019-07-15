fd0f7bc1b6a6698f801b87f67f07050976e375ca
Benjamincapgemini <52694382+Benjamincapgemini@users.noreply.github.com>
2019-07-15 12:04:33 +0200

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -6,28 +6,28 @@ class TestApi(TestCase):
     def setUp(self):
         self.data = Api()
 
-    def test_countries(self):
+    def test_first_country(self):
         self.assertEqual(self.data.countries()[0], "Aruba")
 
-    def test_countries2(self):
+    def test_second_country(self):
         self.assertNotEqual(self.data.countries()[1], "Andorra")
 
-    def test_population(self):
+    def test_population_aruba_1960(self):
         self.assertEqual(self.data.population('Aruba',['1960','1962'])[0], "54211")
 
-    def test_population2(self):
+    def test_population_aruba_1962(self):
         self.assertNotEqual(self.data.population('Aruba',['1960','1962'])[2], "59063")
 
-    def test_countries_pop(self):
+    def test_countries_pop_aruba(self):
         self.assertEqual(self.data.countries_pop(['Aruba','Andorra'],['1960','1962'])[0][1][0], "54211")
 
-    def test_countries_pop2(self):
+    def test_countries_pop_andorra(self):
         self.assertNotEqual(self.data.countries_pop(['Aruba','Andorra'],['1960','1962'])[0][1][1], "57032")
    
-    def test_growth(self):
+    def test_growth_Aruba_and_Afgha_1960_to_1965(self):
         self.assertEqual(self.data.growth(["Aruba","Afghanistan"], ["1960", "1965"]), ["Aruba:3149", "Afghanistan:959347"])
 
-    def test_growth2(self):
+    def test_growth_Aruba_1960_to_1961(self):
         self.assertNotEqual(self.data.growth(["Aruba"], ["1960", "1961"]),["Aruba:3149"])
         
     def test_minpoblacion(self):
