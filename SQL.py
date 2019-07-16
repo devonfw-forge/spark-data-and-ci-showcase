4a4afb8bbe226aebfa464931b87ef53432eb063d
SergioTJ11 <52447686+SergioTJ11@users.noreply.github.com>
2019-07-16 12:18:49 +0200

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -2,18 +2,14 @@ from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-data_tables = create_engine('sqlite:///world-gdp.db')
-
-Session = sessionmaker(bind=data_tables)
-session = Session()
 
 Base = declarative_base()
-
 
 class Gdp(Base):
     '''
    Initializes the Gdp table.
    '''
+
     __tablename__ = 'gdp'
 
     id = Column(Integer, primary_key=True)
@@ -27,6 +23,7 @@ class Countries(Base):
     '''
    Initializes the countries table.
    '''
+
     __tablename__ = 'countries'
 
     CountryCode = Column(String, primary_key=True)
@@ -42,30 +39,51 @@ class Analyse:
         '''
       Returns the list of all the countries.
       '''
-        return [x[0] for x in session.query(Countries.CountryName).all()]
 
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
+        return [x[0] for x in session.query(Countries.CountryName).all()]
+        session.close()
+        
     def countries_code(self):
         '''
       Returns the list of all the countries.
       '''
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
         return [x[0] for x in session.query(Countries.CountryCode).all()]
+        session.close()
 
     def code_to_name(self, code):
         '''
       Returns the name of the country from its country code.
       '''
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
         return session.query(Countries.CountryName).filter_by(CountryCode=code).first()[0]
+        session.close()
 
     def name_to_code(self, name):
         '''
       Returns the code of the country from its country name.
       '''
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
         return session.query(Countries.CountryCode).filter_by(CountryName=name).first()[0]
+        session.close()
 
     def countries_data(self, countries, years):
         '''
       Returns a list of countries with each country a list of years with a dataset (gdp and growth).
       '''
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
+
         countries_data_list = {}
         years_vect = [x for x in range(years[0], years[1] + 1)]
         for country in countries:
@@ -79,11 +97,16 @@ class Analyse:
                     countries_data_list[country][i][2] = 0
 
         return countries_data_list
+        session.close()
 
     def av_gdp(self, countries, years):
         '''
       Returns the average value of the gdp for a country list and a fixed period.
       '''
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
+
         av_list = {}
         for country in countries:
             CC = self.name_to_code(country)
@@ -91,11 +114,16 @@ class Analyse:
             av_list[country] = \
             session.query(func.avg(Gdp.gdp)).filter_by(CountryCode=CC).filter(Gdp.Year.in_(years_vec)).first()[0]
         return av_list
+        session.close()
 
     def av_growth(self, countries, years):
         '''
       Returns the average value of the growth for a country list and a fixed period.
       '''
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
+
         av_list = {}
         for country in countries:
             CC = self.name_to_code(country)
@@ -103,11 +131,16 @@ class Analyse:
             av_list[country] = \
             session.query(func.avg(Gdp.growth)).filter_by(CountryCode=CC).filter(Gdp.Year.in_(years_vect)).first()[0]
         return av_list
+        session.close()
 
     def min_gdp(self, listOfCountries, years):
         '''
       Returns the minimum gdp between countries of listOfCountries for the given period years
       '''
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
+
         list_of_code = {}
         for country in listOfCountries:
             name = self.name_to_code(country)
@@ -118,11 +151,16 @@ class Analyse:
             if elt[1] == '':
                 list_of_code[elt[0]] = 0
         return min(list(list_of_code.values())), list_of_code
+        session.close()
 
     def max_gdp(self, listOfCountries, years):
         '''
       Returns the maximum gdp between countries of listOfCountries for the given period years
       '''
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
+
         list_of_code = {}
         for country in listOfCountries:
             name = self.name_to_code(country)
@@ -133,11 +171,16 @@ class Analyse:
             if elt[1] == '':
                 list_of_code[elt[0]] = 0
         return max(list(list_of_code.values())), list_of_code
+        session.close()
 
     def min_growth(self, listOfCountries, years):
         '''
       Returns the minimum growth between countries of listOfCountries for the given period years
       '''
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
+
         list_of_code = {}
         for country in listOfCountries:
             name = self.name_to_code(country)
@@ -148,11 +191,16 @@ class Analyse:
             if elt[1] == '':
                 list_of_code[elt[0]] = 0
         return min(list(list_of_code.values())), list_of_code
+        session.close()
 
     def max_growth(self, listOfCountries, years):
         '''
       Returns the maximum growth between countries of listOfCountries for the given period years
       '''
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
+
         list_of_code = {}
         for country in listOfCountries:
             name = self.name_to_code(country)
@@ -163,6 +211,5 @@ class Analyse:
             if elt[1] == '':
                 list_of_code[elt[0]] = 0
         return max(list(list_of_code.values())), list_of_code
+        session.close()
 
-
-session.close()
