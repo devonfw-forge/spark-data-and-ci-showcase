ecbe97b71a9c1c6c9e47082877d98809fa841b67
buffongeorges <40161730+buffongeorges@users.noreply.github.com>
2019-07-18 13:58:57 +0200

    Update SQL.py

--- WorldGDP/SQL.py
+++ WorldGDP/SQL.py
@@ -328,11 +328,93 @@ class Analyse:
         Session = sessionmaker(bind=data_tables)
         session = Session()
 
+        gdp = []
+        growth = []
+        prod = []
         f = Fao()
         result_list = {}
         for country in countries_list:
+
+            growth.append(list(self.av_growth([country], years_list).values())[0])
+            gdp.append(list(self.av_gdp([country], years_list).values())[0])
+            prod.append(list(f.average_production([country], years_list, production_type, "Food").values())[0])
+
+
             result_list[country] = ["average growth: "+str(list(self.av_growth([country], years_list).values())[0]),"average gdp: "+str(list(self.av_gdp([country], years_list).values())[0]), "average production: "+ str(list(f.average_production([country], years_list, production_type, "Food").values())[0])]
 
         return result_list
+        print(prod)
         session.close()
+
+    def conclusion_gdp_growth_prod(self, countries_list, year_range_1, year_range_2, production_type):
+        '''
+        Returns a list with the countries and their average gdp, growth and production in years_list
+         '''
+
+        data_tables = create_engine('sqlite:///world-gdp.db')
+        Session = sessionmaker(bind=data_tables)
+        session = Session()
+
+        gdp = []
+        growth = []
+        prod = []
+        diff_growth =[]
+        diff_gdp = []
+        diff_prod = []
+        f = Fao()
+        diff = []
+
+        for country in countries_list:
+
+            growth.append(list(self.av_growth([country], year_range_1).values())[0])
+            gdp.append(list(self.av_gdp([country], year_range_1).values())[0])
+            prod.append(list(f.average_production([country], year_range_1, production_type, "Food").values())[0])
+
+            growth.append(list(self.av_growth([country], year_range_2).values())[0])
+            gdp.append(list(self.av_gdp([country], year_range_2).values())[0])
+            prod.append(list(f.average_production([country], year_range_2, production_type, "Food").values())[0])
+
+        for i in range(len(countries_list)):
+                diff_growth.append(growth[i + 1] - growth[i])
+                diff_gdp.append(gdp[i + 1] - gdp[i])
+                diff_prod.append(prod[i + 1] - prod[i])
+
+        #print(diff_growth)
+        #print(diff_gdp)
+        #print(diff_prod)
+
+        for i in range(len(diff_prod)):
+            diff+=[str(countries_list[i])+": 'growth difference' : "+str(diff_growth[i]) +", 'gdp difference' : "+str(diff_gdp[i]) +", 'production difference' : "+str(diff_prod[i])]
+        return diff
+
+        session.close()
+
+A= Analyse()
+f = Fao()
+
+liste = []
+countries = input ("How many countries: ")
+nb_countries = int(countries)
+for i in range (nb_countries):
+    country = input ("Enter country nº"+str(i+1)+":")
+    liste.append(country)
+
+first_period = input ("Enter year1: ")
+year1 = int(first_period)
+
+second_period = input ("Enter year2: ")
+year2 = int(second_period)
+
+third_period = input ("Enter year3: ")
+year3 = int(third_period)
+
+fourth_period = input ("Enter year4: ")
+year4 = int(fourth_period)
+
+year_range_1 = [year1, year2]
+year_range_2 = [year3, year4]
+
+print(A.av_gdp_growth_prod(liste, year_range_1, f.country_products(liste)))
+print(A.av_gdp_growth_prod(liste, year_range_2, f.country_products(liste)))
+print(A.conclusion_gdp_growth_prod(liste,year_range_1, year_range_2, f.country_products(liste)))
 
