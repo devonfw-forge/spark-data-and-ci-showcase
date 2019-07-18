522a7daaae35c71f4b7bce8782d7db7d162bf776
SergioTJ11 <52447686+SergioTJ11@users.noreply.github.com>
2019-07-18 09:46:19 +0200

    Update FAO.py

--- FAO/FAO.py
+++ FAO/FAO.py
@@ -1,6 +1,9 @@
 import json
 import statistics
+import os
 
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'FAO+database.json')
 
 class Fao:
 
@@ -8,13 +11,14 @@ class Fao:
         '''
         Initializes the class by importing the data from the file
         '''
-        file = open("FAO+database.json", 'r')
+        file = open(my_file, 'r')
         self.dataBase = json.load(file)
         file.close()
 
-    def countries(self):
+    def list_countries(self):
         '''
-        Returns all countries present in the file without doubles.
+        Returns a list of all countries present in the file without doubles.
+        :return: country_list
         '''
         country_list = []
         for element in self.dataBase:
@@ -22,9 +26,11 @@ class Fao:
                 country_list.append(element["Area"])
         return country_list
 
-    def products(self, country):
+    def list_products_country(self, country):
         '''
-        Returns all products for a given country.
+        Returns a list of all the products for a given country.
+        :param country:
+        :return: products_list
         '''
         products_list = []
         for element in self.dataBase:
@@ -32,30 +38,35 @@ class Fao:
                 products_list.append(element["Item"])
         return products_list
 
-    def country_prod(self, countries):
-        '''
-        Returns a list of countries with each country a production list
-        '''
-        final_list = {}
-        for country in countries:
-            final_list[country] = self.products(country)
-        return final_list
+    def list_products_countries(self, country_list):
+        """
+        Returns a list of countries with each country its production list.
+        :param country_list:
+        :return: products_countries_dic
+        """
+        products_countries_dic = {}
+        for country in country_list:
+            products_countries_dic[country] = self.list_products_country(country)
+        return products_countries_dic
 
-    def max(self, country_list, years):
-        '''
+    def max_production(self, country_list, years_range):
+        """
         Returns the maximum of production (production, year and quantity) for some given countries and a fixed date.
-        '''
+        :param country_list:
+        :param years_range:
+        :return: country_dic
+        """
         country_dic = {}
         years_list = []
 
-        for date in range(years[0], years[-1] + 1):
+        for date in range(years_range[0], years_range[-1] + 1):
             years_list.append("Y" + str(date))
 
         for country in country_list:
             country_dic[country] = ["init", "Yinit", 0]
             othermax = []
 
-            for production in self.products(country):
+            for production in self.list_products_country(country):
 
                 for element in self.dataBase:
 
@@ -81,26 +92,30 @@ class Fao:
 
         return country_dic
 
-    def min(self, country_list, years):
+    def min_production(self, country_list, years_range):
         '''
         Returns the minimum of production (production, year and quantity) for some given countries and a fixed date.
+        :param country_list:
+        :param years_range:
+        :return: country_dic
         '''
         country_dic = {}
         years_list = []
 
-        for date in range(years[0], years[-1] + 1):
+        for date in range(years_range[0], years_range[-1] + 1):
             years_list.append("Y" + str(date))
 
         for country in country_list:
             country_dic[country] = ["init", "Yinit", float('inf')]
             othermin = []
 
-            for production in self.products(country):
+            for production in self.list_products_country(country):
 
                 for element in self.dataBase:
 
                     if element["Area"] == country and element["Item"] == production:
                         currentyield = {key: element[key] for key in years_list}
+
 
                         for elt in currentyield.items():
                             if elt[1] == "":
@@ -112,7 +127,7 @@ class Fao:
                         if othermin != [] and currentyield[min(currentyield)] < othermin[-1][2]:
                             othermin = []
 
-                        elif currentyield[min(currentyield)] == country_dic[country][-1]:
+                        if currentyield[min(currentyield)] == country_dic[country][-1]:
                             othermin.append([production, min(currentyield), currentyield[min(currentyield)]])
 
             if othermin != []:
@@ -121,29 +136,101 @@ class Fao:
 
         return country_dic
 
-     # parameters : years -> range of years, Production of the concerned countries specified in listOfCountries
-    def av(self, listOfCountries, years, Production, direction):
-        mean_list = {}
+    def min_production_countries(self, country_list, years_range):
+        '''
+        Returns the minimum of each production (production, year and quantity) for some given countries and a fixed date.
+        :param country_list:
+        :param years_range:
+        :return: country_dic
+        '''
+        country_dic = {}
         years_list = []
 
-        for date in range(years[0], years[-1] + 1):
+        for date in range(years_range[0], years_range[-1] + 1):
             years_list.append("Y" + str(date))
 
-        for country in listOfCountries:
-            for prod in Production:
-                result_list = []
+        for country in country_list:
+            prod_list = []
+            for production in self.list_products_country(country):
+
                 for element in self.dataBase:
-                    if element["Area"] == country and element["Item"] in Production and element["Element"] == direction:
-                    # replace empty item by 0
 
-                        for i in years_list:
-                            if element[i] == "":
-                                element[i] = 0
+                    if element["Area"] == country and element["Item"] == production:
+                        currentyield = {key: element[key] for key in years_list}
 
-                            result_list.append(element[i])
-            print(result_list)
-            mean_list[country] = statistics.mean(result_list)
 
-        return mean_list
+                        for elt in currentyield.items():
+                            if elt[1] == "":
+                                currentyield[elt[0]] = 0
+
+                prod_list.append([production, min(currentyield), currentyield[min(currentyield)]])
+
+        country_dic[country] = prod_list
+
+        return country_dic
+
+    def max_production_countries(self, country_list, years_range):
+        '''
+         Returns the maximum of each production (production, year and quantity) for some given countries and a fixed date.
+         :param country_list:
+         :param years_range:
+         :return: country_dic
+         '''
+        country_dic = {}
+        years_list = []
+
+        for date in range(years_range[0], years_range[-1] + 1):
+            years_list.append("Y" + str(date))
+
+        for country in country_list:
+            prod_list = []
+            for production in self.list_products_country(country):
+
+                for element in self.dataBase:
+
+                    if element["Area"] == country and element["Item"] == production:
+                        currentyield = {key: element[key] for key in years_list}
+
+
+                        for elt in currentyield.items():
+                            if elt[1] == "":
+                                currentyield[elt[0]] = 0
+
+                prod_list.append([production, max(currentyield), currentyield[max(currentyield)]])
+
+        country_dic[country] = prod_list
+
+        return country_dic
+
+    def average_production(self, country_list, years_range, production_type, direction):
+        '''
+        Returns the average of production  for some given countries, a fixed date and a production type given
+        :param country_list:
+        :param years_range:
+        :param production_type:
+        :param direction:
+        :return: av_dic
+        '''
+        av_dic = {}
+        years_list = []
+
+        for date in range(years_range[0], years_range[-1] + 1):
+            years_list.append("Y" + str(date))
+
+        for country in country_list:
+            result_list = []
+            for element in self.dataBase:
+                if element["Area"] == country and element["Item"] == production_type and element["Element"] == direction:
+
+                    for i in years_list:
+                        if element[i] == "":
+                            element[i] = 0
+                        result_list.append(element[i])
+
+            av_dic[country] = statistics.mean(result_list)
+
+        return av_dic
+
+
 
 
