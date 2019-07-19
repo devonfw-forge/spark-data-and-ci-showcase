d9e5284d327ce6162130c3485e510f0a7369954c
SergioTJ11 <52447686+SergioTJ11@users.noreply.github.com>
2019-07-19 09:00:02 +0200

    Update test_fao.py

--- FAO/test_fao.py
+++ FAO/test_fao.py
@@ -6,7 +6,7 @@ class TestFao(TestCase):
     def setUp(self):
         self.data = Fao()
 
-    def test_countries(self):
+    def test_list_countries(self):
         countries = self.data.list_countries()
         self.assertEqual(countries[0], "Afghanistan")
         self.assertEqual(countries[42], "Cyprus")
@@ -14,22 +14,22 @@ class TestFao(TestCase):
         self.assertEqual(countries[137], "Senegal")
         self.assertEqual(len(countries), 174)
 
-
-    def test_products(self):
+    def test_list_products_countries(self):
         countries = self.data.list_countries()
-        self.assertEqual(self.data.list_products_countries(countries[0])[0], "Wheat and products")
-        self.assertEqual(self.data.list_products_countries(countries[-1])[0], "Wheat and products")
+        self.assertEqual(self.data.list_products_countries([countries[0]])[countries[0]][0], "Wheat and products")
+        self.assertEqual(self.data.list_products_countries([countries[-1]])[countries[-1]][0], "Wheat and products")
 
-    def test_min(self):
+    def test_min_production(self):
         countries = self.data.list_countries()
-        self.assertEqual(self.data.min_production_countries([countries[0]], [2010, 2013])[countries[0]][0][-1], 0)
+        self.assertEqual(self.data.min_production([countries[0]], [2010, 2013])[countries[0]][0][-1], 0)
 
-    def test_max(self):
+    def test_max_production(self):
         countries = self.data.list_countries()
-        self.assertEqual(self.data.max_production_countries([countries[0]], [2010, 2013])[countries[0]][-1], 5495)
+        self.assertEqual(self.data.max_production([countries[0]], [2010, 2013])[countries[0]][-1], 5495)
 
     def test_average_production(self):
         countries = self.data.list_countries()
         self.assertEqual(self.data.average_production([countries[0], countries[42]], [1961, 1965], "Wheat and products","Food"),
                          {countries[0] : 1889.8, countries[42] : 67.4})
+
 
