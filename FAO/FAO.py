91725ee632b558870689738b2d839e1d5601a3bf
buffongeorges <40161730+buffongeorges@users.noreply.github.com>
2019-07-19 10:59:14 +0200

    Update FAO.py

--- FAO/FAO.py
+++ FAO/FAO.py
@@ -242,63 +242,7 @@ class Fao:
                     products_list.append(element["Item"])
         return products_list
     
-    def conclusion_gdp_growth_prod(self, countries_list, year_range_1, year_range_2, production_type):
-        '''
-        Returns a list with the countries and their average gdp, growth and production in years_list
-         '''
-
-        data_tables = create_engine('sqlite:///world-gdp.db')
-        Session = sessionmaker(bind=data_tables)
-        session = Session()
-
-        gdp = []
-        growth = []
-        prod = []
-        diff_growth =[]
-        diff_gdp = []
-        diff_prod = []
-        f = Fao()
-        diff = []
-
-        for country in countries_list:
-            if country in f.list_countries():
-                print(country)
-                growth.append(list(self.av_growth([country], year_range_1).values())[0])
-                gdp.append(list(self.av_gdp([country], year_range_1).values())[0])
-                prod.append(list(f.average_production([country], year_range_1, production_type, "Food").values())[0])
-
-                growth.append(list(self.av_growth([country], year_range_2).values())[0])
-                gdp.append(list(self.av_gdp([country], year_range_2).values())[0])
-                prod.append(list(f.average_production([country], year_range_2, production_type, "Food").values())[0])
-
-        for i in range(len(countries_list)):
-                diff_growth.append(growth[i + 1] - growth[i])
-                diff_gdp.append(gdp[i + 1] - gdp[i])
-                diff_prod.append(prod[i + 1] - prod[i])
-
-        #print(diff_growth)
-        #print(diff_gdp)
-        #print(diff_prod)
-
-        for i in range(len(diff_prod)):
-            diff+=[str(countries_list[i])+": 'growth difference' : "+str(diff_growth[i]) + ", production difference : "+str(diff_prod[i])]
-        return diff
-
-        session.close()
-
-A= Analyse()
-f = Fao()
-
-
-
-
-#print(A.conclusion_gdp_growth_prod(liste,year_range_1, year_range_2, f.country_products(liste)))
-
-
-L=A.countries()
-print(L)
-L= ['Afghanistan', 'Albania', 'Algeria', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas, The', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Cabo Verde', 'Cambodia', 'Cameroon', 'Canada', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Congo, Rep.', 'Costa Rica']
-print(A.conclusion_gdp_growth_prod(L,[1985,1989], [1990,1992], f.country_products(L)))
+    
 
 
 
