f68b603c364b0267d00018946ef8724ce4d4b0d6
Torregrosa Jordan <sergio.torregrosa-jordan-external@capgemini.com>
2019-08-08 10:01:19 +0200

    updating demo files

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,12 +2,21 @@ pipeline{
     agent any
     
     stages{
+
+        stage('Create docker contaimer'){
+            steps{
+                script {
+                    bat "docker run -it --name SparkJenkins -p 8887:8888 -v ~:/home/jovyan jupyter/pyspark-notebook"
+
+                }
+        }
         
         stage('test'){
             steps{
                 script {
-                    bat "docker exec -i 15e9343d9b88 /usr/local/spark-2.4.3-bin-hadoop2.7/bin/spark-submit test/FAO/test_FAO_spark.py"
-                    bat "docker exec -i 15e9343d9b88 /usr/local/spark-2.4.3-bin-hadoop2.7/bin/spark-submit test/WorldGDP/test-SQLiteNotebook.py"
+                    bat "docker exec -i SparkJenkins /usr/local/spark-2.4.3-bin-hadoop2.7/bin/spark-submit test/FAO/test_FAO_spark.py"
+                    bat "docker exec -i SparkJenkins /usr/local/spark-2.4.3-bin-hadoop2.7/bin/spark-submit test/WorldGDP/test-SQLiteNotebook.py"
+
                 }
         }
     }
---
+++ notebooks/Unemployment.py
@@ -0,0 +1,59 @@
+from pyspark.sql import SparkSession, functions as F
+from pyspark.sql.types import DoubleType,IntegerType
+import plotly.graph_objects as go
+import pandas as pd
+
+class Employment():
+
+    def __init__(self):
+        self.spark = SparkSession \
+            .builder \
+            .appName("Employment App") \
+            .getOrCreate()
+        
+        self.df_unemployment = self.spark \
+            .read \
+            .format('csv') \
+            .options(header='true', inferSchema='true', delimiter=';') \
+            .load('Book2.csv')
+        
+    def extract_info(self, years_frame, countries_list ):
+        years = [str(year) for year in range(years_frame[0],years_frame[-1]+1)]
+        
+        return  self.df_unemployment\
+                .fillna(0)\
+                .select([c for c in self.df_unemployment.columns if c in ['CountryName'] + years]) \
+                .filter(self.df_unemployment.CountryName.isin(countries_list))\
+                    
+    def add_groups(self,Data, geo_zone):
+        Data_extended = Data.withColumn('GroupName', Data.CountryName )
+        return Data_extended.replace(geo_zone,1,'GroupName')
+    
+    def groups_data(self, Data, years_frame):
+        dicc = {}
+        for year in range(years_frame[0],years_frame[-1]+1):
+            dicc[str(year)] = 'sum'
+        for column in Data.columns[1:-1]:
+            
+            Data = Data.withColumn(column, F.regexp_replace(column,',','.'))
+          
+        return Data.groupby('GroupName').agg(dicc).orderBy('GroupName')   
+        
+    
+    def plot_unemployment(self,Data, years_frame):
+            
+            dataframePanda = Data.toPandas()
+
+            years = [str(year) for year in range(years_frame[0],years_frame[-1]+1)]
+            fig = go.Figure()
+            zone_list_plot = {'Asian Tigers':'red', 'BRICS':'blue', 'China':'yellow', 'Europe':'green', 'Japan':'magenta', 'US':'black'}
+            for i,zone in enumerate(list(zone_list_plot.keys())):
+                
+                unemployment = dataframePanda.iloc[i][1:-1]
+                fig.add_trace(go.Scatter(x=years, y=unemployment.iloc[::-1], name=zone,
+                                         line_color=zone_list_plot[zone]))
+
+            fig.update_layout(title_text='Unemployment percentage from {} to {}'.format(years_frame[0], years_frame[1]),
+                                  xaxis_rangeslider_visible=True)
+            fig.show()          
+            
