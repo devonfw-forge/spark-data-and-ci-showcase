47b2fb4e17c37d4f89a7aaa44ff4a21a0ad26c17
Torregrosa Jordan <sergio.torregrosa-jordan-external@capgemini.com>
2019-08-13 11:57:29 +0200

    updating gitignore

--- employment/Unemployment.py
+++ employment/Unemployment.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession, functions as F
 from pyspark.sql.types import DoubleType,IntegerType
 import plotly.graph_objects as go
 import pandas as pd
+from typing import Dict 
+from pyspark.sql.types import DataType
 
 class Employment():
 
@@ -9,7 +11,7 @@ class Employment():
         self.spark = sparkSession         
         self.df_unemployment = data
         
-    def extract_info(self, years_frame, countries_list ):
+    def extract_info(self, years_frame: list, countries_list: list ) -> DataType:
         years = [str(year) for year in range(years_frame[0],years_frame[-1]+1)]
         
         return  self.df_unemployment\
@@ -17,11 +19,11 @@ class Employment():
                 .select([c for c in self.df_unemployment.columns if c in ['CountryName'] + years]) \
                 .filter(self.df_unemployment.CountryName.isin(countries_list))\
                     
-    def add_groups(self,Data, geo_zone):
+    def add_groups(self,Data:DataType, geo_zone:dict) -> DataType:
         Data_extended = Data.withColumn('GroupName', Data.CountryName )
         return Data_extended.replace(geo_zone,1,'GroupName')
     
-    def groups_data(self, Data, years_frame):
+    def groups_data(self, Data: DataType, years_frame:list) -> DataType:
         dicc = {}
         for year in range(years_frame[0],years_frame[-1]+1):
             dicc[str(year)] = 'sum'
@@ -32,7 +34,7 @@ class Employment():
         return Data.groupby('GroupName').agg(dicc).orderBy('GroupName')   
         
     
-    def plot_unemployment(self,Data, years_frame):
+    def plot_unemployment(self,Data:DataType, years_frame:list) -> None:
             
             dataframePanda = Data.toPandas()
 
